a0bc65b2fbe29af9b18a73ad7deca07bb5ee5581
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2019-09-28 13:28:05 +0200

    Update mod_articles.php

--- web/module/mod_articles.php
+++ web/module/mod_articles.php
@@ -12,18 +12,7 @@ $user=new User();
 foreach ($article->getData() as $key => $value) : ?>
 	<article class="article" value="<?= $value['post_id']; ?>">
 		<h1><a href="article/<?= $value['post_id']; ?>"><?= $value['title'] ?></a></h1>
-	<!-- TEST LIKE
- 	<?php
-		if(isset($_POST['like'])){
-			$article->like($value['post_id']);
-			dump($value['post_id']);
-		}
-		?>
-		<form action="" method="post">
-			<button type="submit" data-id="<?=$value['post_id']?>" name="like">like <?= $value['like']; ?></button>			
-		</form>	
-	
-	-->
+
 		<?php foreach ($user->getData() as $k => $v) :
 			if ($v["user_id"] == $value["user_id"]) : ?>
 				<small><?= $v["username"] ?></small>
@@ -32,9 +21,7 @@ foreach ($article->getData() as $key => $value) : ?>
 			if ($v['parent_id'] == $value['post_id']) : ?>
 				<small><?= $v['comm'] ?> Commentaire</small>
 			<?php endif;endforeach;?>
-		<!-- TEST Hashtag
-			<p><?= preg_replace("/\#/","<a href=''>#</a>",$value['post']); ?></p>	
-		-->	
+
 		<p><?= $value['post']?></p>
 		<?php
 		preg_match("/mp4|MOV|wmv|mkv|avi|m4v|webm/i",$value["img"],$video);
