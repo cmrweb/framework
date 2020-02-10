a8079d1b39c3e6883ab326e028c2102260cf0e58
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-10 11:56:05 +0100

    init

--- web/module/init.php
+++ web/module/init.php
@@ -1,32 +1,32 @@
 <?php
 
-$projectName = preg_replace("/\//","",$_ENV['ROOT_PATH']);
+$projectName = preg_replace("/\//", "", $_ENV['ROOT_PATH']);
 $dbHOST = $_ENV['DB_HOST'];
 $dbNAME = $_ENV['DB_NAME'];
 $dbUSER = $_ENV['DB_USER'];
 $dbPASS = $_ENV['DB_PASS'];
 
-if(isset($_POST['send'])){
-    //réecrire .env
-    if(!empty($_POST['dbHost']) && !empty($_POST['dbName']) && !empty($_POST['dbUser']) && !empty($_POST['username']) && !empty($_POST['pwd'])){
-        $user = $_POST['username'];
-        $pwd = password_hash($_POST['pwd'],PASSWORD_BCRYPT);
-        $dbHOST = $_POST['dbHost'];
-        $dbNAME = $_POST['dbName'];
-        $dbUSER = $_POST['dbUser'];
-        $dbPASS = $_POST['dbPwd'];
-        $envContent = "
+if (isset($_POST['send'])) {
+  //réecrire .env
+  if (!empty($_POST['dbHost']) && !empty($_POST['dbName']) && !empty($_POST['dbUser']) && !empty($_POST['username']) && !empty($_POST['pwd'])) {
+    $user = $_POST['username'];
+    $pwd = password_hash($_POST['pwd'], PASSWORD_BCRYPT);
+    $dbHOST = $_POST['dbHost'];
+    $dbNAME = $_POST['dbName'];
+    $dbUSER = $_POST['dbUser'];
+    $dbPASS = $_POST['dbPwd'];
+    $envContent = "
         APP_ENV=\"dev\"
         DB_HOST=\"{$dbHOST}\"
         DB_NAME=\"{$dbNAME}\"
         DB_USER=\"{$dbUSER}\"
         DB_PASS=\"{$dbPASS}\"
         ROOT_PATH=\"/{$_POST['projectName']}\"";
-        //dump($envContent);
-        file_put_contents(".env",$envContent);   
-    
+    //dump($envContent);
+    file_put_contents(".env", $envContent);
+
     //create database if not exist
-    $pdo = new PDO("mysql:host={$dbHOST};","{$dbUSER}","{$dbPASS}");
+    $pdo = new PDO("mysql:host={$dbHOST};", "{$dbUSER}", "{$dbPASS}");
     $createDB = $pdo->prepare("CREATE DATABASE IF NOT EXISTS {$dbNAME}");
     $createDB->execute();
     echo "La base de donnée {$dbNAME} à été créer.";
@@ -55,66 +55,85 @@ if(isset($_POST['send'])){
       `category` int(11) DEFAULT NULL,
       PRIMARY KEY (`id`)
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-    
+
+    DROP TABLE IF EXISTS `cmr_follow`;
+    CREATE TABLE IF NOT EXISTS `cmr_follow` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `user_id` int(11) NOT NULL,
+      `follow_id` int(11) NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+    DROP TABLE IF EXISTS `chat`;
+    CREATE TABLE IF NOT EXISTS `chat` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nom` varchar(100) NOT NULL,
+      `message` text NOT NULL,
+      `date` datetime NOT NULL DEFAULT '2019-10-03 00:00:00',
+      `sendby` int(11) NOT NULL,
+      `sendto` int(11) DEFAULT '0',
+      PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
     DROP TABLE IF EXISTS `online_user`;
     CREATE TABLE IF NOT EXISTS `online_user` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `user_id` text NOT NULL,
       `time` datetime DEFAULT NULL,
       PRIMARY KEY (`id`)
-    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;COMMIT;");  
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8;COMMIT;");
     $tableUser->execute();
 
     //reecriture des routes
-    $route = preg_replace("/module\/init/","pages/home",file_get_contents("web/module/route.php"));
+    $route = preg_replace("/module\/init/", "pages/home", file_get_contents("web/module/route.php"));
     //dump($route);
     file_put_contents("web/module/route.php", $route);
-}
+  }
 }
 
 
 ?>
 <style>
-label {
+  label {
     display: block
-}
+  }
 </style>
 <form method="post" class='large primary formContainer'>
-<h1>Initialiation du projet</h1>
-    <div class="form">
-       <label for="projectName">Nom du projet</label>
-    <input class="input" type="text" id="projectName" name="projectName" value="<?=$projectName?>"> 
-    </div>
-    
-    <div class="form">
+  <h1>Initialiation du projet</h1>
+  <div class="form">
+    <label for="projectName">Nom du projet</label>
+    <input class="input" type="text" id="projectName" name="projectName" value="<?= $projectName ?>">
+  </div>
+
+  <div class="form">
     <label for="dbHost">Hote de la Base de données</label>
-    <input class="input" type="text" id="dbHost" name="dbHost" value="<?=$dbHOST?>">
-    </div>
+    <input class="input" type="text" id="dbHost" name="dbHost" value="<?= $dbHOST ?>">
+  </div>
 
-    <div class="form">
+  <div class="form">
     <label for="dbName">Nom de la Base de données</label>
-    <input class="input" type="text" id="dbName" name="dbName" value="<?=$dbNAME?>">
-    </div>
+    <input class="input" type="text" id="dbName" name="dbName" value="<?= $dbNAME ?>">
+  </div>
 
-    <div class="form">
+  <div class="form">
     <label for="dbUser">Nom d'utilisateur de la Base de données</label>
-    <input class="input" type="text" id="dbUser" name="dbUser" value="<?=$dbUSER?>">
-    </div>
+    <input class="input" type="text" id="dbUser" name="dbUser" value="<?= $dbUSER ?>">
+  </div>
 
-    <div class="form">
+  <div class="form">
     <label for="dbPwd">Mot de passe de la Base de données</label>
-    <input class="input" type="text" id="dbPwd" name="dbPwd" value="<?=$dbPASS?>">
-    </div>
+    <input class="input" type="text" id="dbPwd" name="dbPwd" value="<?= $dbPASS ?>">
+  </div>
 
-    <div class="form">
+  <div class="form">
     <label for="username">Nom d'utilisateur</label>
     <input class="input nameSecure" type="text" id="username" name="username">
-    </div>
+  </div>
 
-    <div class="form">
+  <div class="form">
     <label for="pwd">Mot de passe</label>
     <input class="input passSecure" type="password" id="pwd" name="pwd">
-    </div>
+  </div>
 
-    <button class="btn success large center m4" name="send">Valider</button>
+  <button class="btn success large center m4" name="send">Valider</button>
 </form>
