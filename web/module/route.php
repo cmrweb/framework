9e52a759593262f3e0a6d708723339efb0dbee17
cmrweb <33638879+tykrasta@users.noreply.github.com>
2020-02-13 13:24:50 +0100

    clean

--- web/module/route.php
+++ web/module/route.php
@@ -12,10 +12,6 @@ switch ($url) {
     case $url[0] == 'dev' and empty($url[1]):
         require 'web/pages/dev.php';
         break;
-    case $url[0] == 'docs' and empty($url[1]):
-        require 'web/pages/controller/c_docs.php';
-        require 'web/pages/docs.php';
-        break;
 
     case $url[0] == 'edit' and empty($url[1]):
         require 'web/pages/controller/c_post.php';
@@ -39,6 +35,7 @@ switch ($url) {
     case $url[0] == 'ajax':
         require 'web/pages/controller/c_ajax.php';
         break;
+
 
 case $url[0] == 'user' and empty($url[1]):
     require 'web/pages/controller/c_user.php';
