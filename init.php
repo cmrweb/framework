5b41d70d9086af2b097da66cb776c8fef7d3edbc
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-14 13:31:10 +0100

    Update init.php

--- init.php
+++ init.php
@@ -13,7 +13,11 @@ DB_PASS=\"{$dbPASS}\"
 ROOT_PATH=\"/{$projectName}\"";
 file_put_contents(".env", $envContent);
 echo ".env generer";
-echo $envContent;
+//echo $envContent;
 $cli = preg_replace("/cmrweb/", $projectName, file_get_contents("lib/cli/cmr.bat"));
     file_put_contents("lib/cli/cmr.bat", $cli);
-echo "init cli";
+echo "init cli\n";
+echo "cd $projectName\n";
+echo "cd lib\n";
+echo "cli/cmr\n";
+echo "cmr start\n";
