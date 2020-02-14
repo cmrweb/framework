c977fcec1202fbb77bca9288b4e87f97b3580d77
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-14 21:47:38 +0100

    Update c_post.php

--- web/pages/controller/c_post.php
+++ web/pages/controller/c_post.php
@@ -6,47 +6,31 @@ $msg = "";
 $Post = new Post();
 if (isset($_POST['addPost'])) {
     if (!empty($_FILES['img']["size"])) {
-        if ($_FILES['img']["size"] <= 500000) {
-            $target_dir =  "../" . ROOT_DIR . IMG_DIR."upload/";
-            $bytes = random_bytes(5);
-            $ext = pathinfo(basename($_FILES['img']["name"]), PATHINFO_EXTENSION);
-            $target_file = bin2hex($bytes);
-            $uploadName = strtolower($target_file . '.' . $ext);
-            $fileDbName = strtolower($target_file . '.' . $ext);
-            if (is_uploaded_file($_FILES['img']["tmp_name"]))
-                if (move_uploaded_file($_FILES['img']["tmp_name"],  $target_dir . $uploadName)) {
-                    $Post->setData(["user_id" => $userid, "title" => $_POST['title'], "post" => $_POST['post'], "img" => "$fileDbName"]);
-                }
-        } else {
-            $msg = "image trop lourde";
+        $fileDbName = uploadImg($_FILES['img']);
+        if (move_uploaded_file($_FILES['img']["tmp_name"], "../" . ROOT_DIR . IMG_DIR."upload/" . $fileDbName)) {
+            $Post->setData(["user_id" => $userid, "title" => $_POST['title'], "post" => $_POST['post'], "img" => "$fileDbName"]);
+        }else{
+            echo "une erreur est survenu";
         }
     } else {
         $Post->setData(["user_id" => $userid, "title" => $_POST['title'], "post" => $_POST['post']]);
     }
-    header("Location: ./edit");
+    header("Location: edit");
 }
 if (isset($_POST['update'])) {
     if (!empty($_FILES['img']["size"])) {
-        if ($_FILES['img']["size"] <= 500000) {
-            $target_dir =  "../" . ROOT_DIR . IMG_DIR."upload/";
-            $bytes = random_bytes(5);
-            $ext = pathinfo(basename($_FILES['img']["name"]), PATHINFO_EXTENSION);
-            $target_file = bin2hex($bytes);
-            $uploadName = strtolower($target_file . '.' . $ext);
-            $fileDbName = strtolower($target_file . '.' . $ext);
-            if (is_uploaded_file($_FILES['img']["tmp_name"]))
-                if (move_uploaded_file($_FILES['img']["tmp_name"],  $target_dir . $uploadName)) {
-                    $Post->update(["user_id" => $userid, "title" => $_POST['title'], "post" => $_POST['post'], "img" => "$fileDbName"], "id=" . $_POST['id']);
-                }
-        } else {
-            $msg = "image trop lourde";
+        $fileDbName = uploadImg($_FILES['img']);
+        if (move_uploaded_file($_FILES['img']["tmp_name"],  "../" . ROOT_DIR . IMG_DIR."upload/" . $fileDbName)) {
+            $Post->update(["user_id" => $userid, "title" => $_POST['title'], "post" => $_POST['post'], "img" => "$fileDbName"], "id=" . $_POST['id']);
+        }else{
+            echo "une erreur est survenu";
         }
     } else {
         $Post->update(["user_id" => $userid, "title" => $_POST['title'], "post" => $_POST['post']], "id=" . $_POST['id']);
     }
-    header("Location: ./edit");
+    header("Location: edit");
 }
 if (isset($_POST['delete'])) {
     $Post->delete($_POST['id']);
-    header("Location: ./edit");
+    header("Location: edit");
 }
