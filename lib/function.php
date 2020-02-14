f22ae4aeae815de385f459f319906abe6aca1d7a
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-14 21:47:20 +0100

    Update function.php

--- lib/function.php
+++ lib/function.php
@@ -17,3 +17,15 @@ function needAdmin(){
 function dump($var){
   echo "<pre>";var_dump($var);echo"</pre>";
 }
+
+function uploadImg($img){
+  if ($img["size"] <= 500000) {
+    $bytes = random_bytes(5);
+    $ext = pathinfo(basename($img["name"]), PATHINFO_EXTENSION);
+    $target_file = bin2hex($bytes);
+    $uploadName = strtolower($target_file . '.' . $ext);
+    if (is_uploaded_file($img["tmp_name"]))
+    return $uploadName;
+  }else
+  return "l'image est trop lourde";
+}
