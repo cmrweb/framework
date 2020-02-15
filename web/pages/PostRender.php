6651607ad10bc79d9477954f76604d961dc9e71b
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-15 13:29:44 +0100

    add post comment

--- web/pages/PostRender.php
+++ web/pages/PostRender.php
@@ -2,6 +2,7 @@
 dump($Post->getData());
 if ($Post->getData()) : ?>
     <section class="large light">
+
         <?= $html->h('1', 'Read');
             foreach ($Post->getData() as $key => $value) :
                 echo $html->code("section",
@@ -9,8 +10,16 @@ if ($Post->getData()) : ?>
                     $html->a((isset($id))?"":"post/". $value['id'],$html->h(2, $value['title'])).
                     $html->p($value['post']),
                     (isset($id))?"article large light":"articles large light");
-                    if(isset($id)):?>
+                    if($comments)if($comments->getData())
+                    foreach ($comments->getData() as $comment){
+                        echo $html->code("section",
+                        $html->img(ROOT_DIR . IMG_DIR."upload/" . $comment['img'], $comment['img'], "medium center") .
+                        $html->a((isset($id))?"":"post/". $comment['id'],$html->h(2, $comment['title'])).
+                        $html->p($comment['post']),
+                        (isset($id))?"article large light":"articles large light");
+                     } if(isset($id)):?>
                       <form method="post" class="form large light" enctype="multipart/form-data">
+                          <input type="hidden" name="parent_id" value="<?=$value['id']?>">
                           <input type="text" class="input" name="title">
                           <textarea class="input" name="post" cols="30" rows="3"></textarea>
                           <input type="file" name="img">
--- web/pages/controller/c_postRender.php
+++ web/pages/controller/c_postRender.php
@@ -1,6 +1,22 @@
 <?php
 if(isset($id)){
-    $Post = new Post("id=".$id);    
+    $Post = new Post("id=".$id); 
+    $comments = new Post("parent_id=".$id);
 }else{
-    $Post = new Post();
+    $Post = new Post("parent_id IS NULL");
+    $comments = null;
 }
+if (isset($_POST['reponse'])) {
+    if(!empty($_POST['title'])&&!empty($_POST['parent_id'])&&!empty($_POST['post']))
+    if (!empty($_FILES['img']["size"])) {
+        $fileDbName = uploadImg($_FILES['img']);
+        if (move_uploaded_file($_FILES['img']["tmp_name"], "../" . ROOT_DIR . IMG_DIR."upload/" . $fileDbName)) {
+            $Post->setData(["parent_id" => $_POST['parent_id'],"user_id" => 0, "title" => $_POST['title'], "post" => $_POST['post'], "img" => "$fileDbName"]);
+        }else{
+            echo "une erreur est survenu";
+        }
+    } else {
+        $Post->setData(["parent_id" => $_POST['parent_id'],"user_id" => 0, "title" => $_POST['title'], "post" => $_POST['post']]);
+    }
+    header("Location: ./$id");
+}  
