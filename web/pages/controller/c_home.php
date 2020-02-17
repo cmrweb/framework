a1ab9138849c9b08f9c767645c3abff8033eead1
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-17 23:45:37 +0100

    Update c_home.php

--- web/pages/controller/c_home.php
+++ web/pages/controller/c_home.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['init'])){
         //reecriture des routes
-        $route = preg_replace("/pages\/home/","module/init",file_get_contents("web/module/route.php"));
+        $route = preg_replace("/home/","init",file_get_contents("web/module/route.php"));
         //dump($route);
         file_put_contents("web/module/route.php", $route);
 }
