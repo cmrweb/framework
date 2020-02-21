f87dfadbdcf641c83e21266abf6cb2ac65301ae3
cmrwebdev <33638879+cmrweb@users.noreply.github.com>
2020-02-21 16:03:27 +0100

    Update dev.php

--- web/pages/dev.php
+++ web/pages/dev.php
@@ -4,5 +4,5 @@
 //require "web/module/contact.list.php";
 //require "web/module/cmr.bot.php";
 
-Vue::template("./web/pages/html/test.html", "./web/pages/controller/c_dev.php");
+Vue::template("./web/pages/html/$file.html", "./web/pages/controller/c_$file.php");
 ?>
